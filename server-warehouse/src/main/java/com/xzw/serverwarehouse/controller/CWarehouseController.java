5d4b4abab3c562df1fdbcd951387979cb43316c7
xzw <180327904@qq.com>
2021-02-10 22:50:51 +0800

    修改结构 增加依赖

--- server-warehouse/src/main/java/com/xzw/serverwarehouse/controller/CWarehouseController.java
+++ server-warehouse/src/main/java/com/xzw/serverwarehouse/controller/CWarehouseController.java
@@ -1,9 +1,15 @@
 package com.xzw.serverwarehouse.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.xzw.serverwarehouse.entity.CWarehouse;
+import com.xzw.serverwarehouse.service.ICWarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cWarehouse")
 public class CWarehouseController {
+    @Resource
+    ICWarehouseService service;
 
+    @RequestMapping("/getOne")
+    public CWarehouse getOne(CWarehouse warehouse){
+        QueryWrapper<CWarehouse> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(CWarehouse::getId,warehouse.getId());
+        return service.getOne(wrapper);
+    }
 }
--- server-warehouse/src/main/java/com/xzw/serverwarehouse/entity/CWarehouse.java
+++ server-warehouse/src/main/java/com/xzw/serverwarehouse/entity/CWarehouse.java
@@ -3,14 +3,15 @@ package com.xzw.serverwarehouse.entity;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * <p>
@@ -67,7 +68,9 @@ public class CWarehouse implements Serializable {
     /**
      * 增加时间
      */
-    @TableField("InstallDate")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @TableField(value = "InstallDate",update = "now()")
     private Date InstallDate;
 
     /**
